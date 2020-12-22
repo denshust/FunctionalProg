ca36a36b21dc9d5948fe3230f2e05de223b7ab05
denshust <50724048+denshust@users.noreply.github.com>
2020-12-22 02:50:23 +0200

    Додав сайт, щоб вже працював

---
+++ main.js
@@ -0,0 +1,51 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+document.getElementById('lab1').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%201%2C2%2C3/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20%E2%84%961%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('lab2').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%201%2C2%2C3/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20%E2%84%962%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('lab3').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%201%2C2%2C3/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20%E2%84%963%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('lab4').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%204%2C5%2C6/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20%E2%84%964%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('lab5').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%204%2C5%2C6/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20%E2%84%965%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('lab6').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%204%2C5%2C6/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20%E2%84%966%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('lab7').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%207%2C8%2Cc1/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20%E2%84%967%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('lab8').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%207%2C8%2Cc1/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20%E2%84%968%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('с1').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%207%2C8%2Cc1/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20Scala%20%E2%84%961%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('с2').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%20c2%2Cc3%2Cc4/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20Scala%20%E2%84%962%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('с3').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%20c2%2Cc3%2Cc4/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20Scala%20%E2%84%963%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+document.getElementById('с4').onclick = function()
+{
+    window.open("https://htmlpreview.github.io/?https://github.com/denshust/FunctionalProg/blob/main/FP%20c2%2Cc3%2Cc4/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D1%96%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C%D0%BD%D0%B5%20%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D1%83%D0%B2%D0%B0%D0%BD%D0%BD%D1%8F%20Scala%20%E2%84%964%20%D0%A8%D1%83%D1%81%D1%82%20%D0%94%D0%B5%D0%BD%D0%B8%D1%81.html");
+}
+
